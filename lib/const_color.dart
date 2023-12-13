fe74944f443f0b36e0397ada0900f0f89bdf0a91
Takumi Yamada <takumiyamada@TakumiMacBook-Air.local>
2023-12-13 11:33:34 +0900

    定数Colorを追加

---
+++ lib/const_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFFFB471F);
+
+const Color baseColor = Color(0xFFE1E4EA);
+
+const Color black = Color(0xFF161618);
+
+const Color white = Colors.white;
+
+const Color grey = Color(0xFFA3A7A4);
+
+const Color navyBlue = Color(0xFF2D3339);
