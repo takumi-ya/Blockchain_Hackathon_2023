04094ea03d6251ae7e39a59f19dfee44c991f7c3
Takumi Yamada <takumiyamada@TakumiMacBook-Air.local>
2023-12-14 23:18:08 +0900

    constファイルを作った

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:routemaster/routemaster.dart';
-import 'package:voting_app/const_color.dart';
+import 'package:voting_app/const/const_color.dart';
 import 'package:voting_app/route.dart';
 
 void main() {
