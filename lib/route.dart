087ef15964314aeb2586766c6befb3d50ce630ad
Takumi Yamada <takumiyamada@TakumiMacBook-Air.local>
2023-12-14 11:22:06 +0900

    一旦作った、要修正

--- lib/route.dart
+++ lib/route.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:routemaster/routemaster.dart';
-import 'package:voting_app/screen.dart';
+import 'package:voting_app/voting_screen.dart';
 
 final routemaster = RoutemasterDelegate(routesBuilder: (context) => routes);
 
 final routes = RouteMap(
   routes: {
-    '/': (route) => MaterialPage(
-          child: Screen(),
+    '/': (route) => const MaterialPage(
+          child: VotingScreen(),
         )
   },
 );
