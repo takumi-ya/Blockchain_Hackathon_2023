0cf9c391066e7276ff1d62e3d36a8074e04044d6
Takumi Yamada <takumiyamada@TakumiMacBook-Air.local>
2023-12-15 00:34:15 +0900

    ようやく目的のものに()

--- lib/src/voting/mobile_voting_screen.dart
+++ lib/src/voting/mobile_voting_screen.dart
@@ -24,85 +24,118 @@ class MobileVotingScreen extends StatelessWidget {
       backgroundColor: baseColor,
       body: Padding(
         padding: EdgeInsets.symmetric(
-          horizontal: deviceHeight * 0.1,
-          vertical: deviceWidth * 0.05,
+          horizontal: deviceWidth * 0.05,
+          vertical: deviceHeight * 0.05,
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Container(
-                  width: deviceWidth * 0.2,
-                  height: deviceWidth * 0.15,
-                  decoration: BoxDecoration(
-                    color: primaryColor,
-                    borderRadius: BorderRadius.circular(deviceWidth * 0.01),
-                  ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: deviceWidth * 0.2,
+                height: deviceWidth * 0.1,
+                decoration: BoxDecoration(
+                  color: primaryColor,
+                  borderRadius: BorderRadius.circular(deviceWidth * 0.01),
+                ),
+                child: FittedBox(
                   child: Text(
                     '投票',
-                    style: TextStyle(color: white, fontSize: deviceWidth * 0.1),
+                    style: TextStyle(
+                      color: white,
+                      fontSize: deviceWidth * 0.08,
+                    ),
                   ),
                 ),
-                const Spacer(),
-              ],
-            ),
-            Container(
-              height: deviceWidth * 0.5,
-              decoration: BoxDecoration(
-                color: white,
-                borderRadius: BorderRadius.circular(deviceWidth * 0.05),
               ),
-              child: Text(
-                'きのこの山と\nたけのこの里\nどっちが好き？',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: navyBlue,
-                  fontSize: deviceWidth * 0.1,
-                ),
+              SizedBox(
+                height: deviceHeight * 0.05,
               ),
-            ),
-            // SizedBox(
-            //   height: deviceWidth * 0.2,
-            // ),
-            Container(
-              width: deviceWidth * 0.6,
-              height: deviceWidth * 0.2,
-              decoration: BoxDecoration(
-                color: white,
-                borderRadius: BorderRadius.circular(deviceWidth * 0.05),
-              ),
-              child: FittedBox(
-                child: Text(
-                  'きのこの山',
+              ContainerWithText(
+                width: double.infinity,
+                height: deviceWidth * 0.5,
+                radius: deviceWidth * 0.05,
+                text: Text(
+                  'きのこの山と\nたけのこの里\nどっちが好き？',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color: navyBlue,
                     fontSize: deviceWidth * 0.1,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-            ),
-            Container(
-              width: deviceWidth * 0.6,
-              height: deviceWidth * 0.2,
-              decoration: BoxDecoration(
-                color: white,
-                borderRadius: BorderRadius.circular(deviceWidth * 0.05),
+              SizedBox(
+                height: deviceWidth * 0.2,
               ),
-              child: FittedBox(
-                child: Text(
-                  'たけのこの里',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: navyBlue,
-                    fontSize: deviceWidth * 0.1,
+              Align(
+                alignment: Alignment.center,
+                child: ContainerWithText(
+                  width: deviceWidth * 0.8,
+                  height: deviceWidth * 0.2,
+                  radius: deviceWidth * 0.05,
+                  text: Text(
+                    'きのこの山',
+                    style: TextStyle(
+                      color: navyBlue,
+                      fontSize: deviceWidth * 0.1,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: deviceWidth * 0.1,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: ContainerWithText(
+                  width: deviceWidth * 0.8,
+                  height: deviceWidth * 0.2,
+                  radius: deviceWidth * 0.05,
+                  text: Text(
+                    'たけのこの里',
+                    style: TextStyle(
+                      color: navyBlue,
+                      fontSize: deviceWidth * 0.1,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class ContainerWithText extends StatelessWidget {
+  const ContainerWithText({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.radius,
+    required this.text,
+  });
+
+  final double width;
+  final double height;
+  final double radius;
+  final Widget text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: white,
+        borderRadius: BorderRadius.circular(radius),
+      ),
+      child: Center(
+        child: text,
       ),
     );
   }
